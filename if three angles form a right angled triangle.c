b58a8384b6cd8118f445d2be192ffa67757cb69e
Sri charan <itsmeinsanes@gmail.com>
2025-11-26 11:59:27 +0530

    Update and rename even.c to if three angles form a right angled triangle.c

--- if three angles form a right angled triangle.c
+++ if three angles form a right angled triangle.c
@@ -1,26 +1,14 @@
-/* Write a program to read three integer values from the keyboard and display the 
- output stating that they are the sides of right-angled triangle.
-Sample input=
-5
-6
-5
-Output=
-Triangle is Not a Right Angle Triangle
-
- 
-
-*/
 #include<stdio.h>
 int main ()
 {
     int a,b,c;
-    printf("enter the length of the first side:");
+    printf("enter the length of the first side:"); //read the values of the angles
     scanf("%d",&c);
     printf("enter the lenght of the second side:");
     scanf("%d",&a);
     printf("enter the length of the third side:");
     scanf("%d",&b);
-    if((c*c)==(a*a)+(b*b)||(a*a)==(c*c)+(b*b)||(b*b)==(a*a)+(c*c))
+    if((c*c)==(a*a)+(b*b)||(a*a)==(c*c)+(b*b)||(b*b)==(a*a)+(c*c)) //the formula of right angled triangle (a^2=b^2+c^2 where a,b,c are the sides) is being used to check  
     {
         printf("The given sides represent a right angled triangle");
     }
